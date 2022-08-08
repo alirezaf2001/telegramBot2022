96f9d8d77722c3620ec98acbe88856c8d8d515ba
alirezaf2001 <alireza.f220@gmail.com>
2022-08-08 19:26:34 +0430

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ import time
 TOKEN = "5439725443:AAHwW8XYKyiJd8wyQQlwbIOozTNxjpecBTI"
 bot = telebot.TeleBot(TOKEN)
 
-print("bot is running successfuly!")
+@bot.message_handler(commands=["start"])
+def start(message):
+    print(message.text)
+
+@bot.message_handler(commands=["hello", "hi"])
+def hello(message):
+    bot.send_message(message.chat.id, "Hello mother fucker😏🍆")
+
+while True:
+    try:
+        bot.polling()
+    except:
+        time.sleep(5)
