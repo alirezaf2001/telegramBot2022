7b94f1b2fd5dfe088fb764696311f9db217a9778
alirezaf2001 <alireza.f220@gmail.com>
2022-08-08 19:40:26 +0430

    update 8/8/2022

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def start(message):
 def hello(message):
     bot.send_message(message.chat.id, "Hello mother fucker😏🍆")
 
+@bot.message_handler(commands=["img"])
+def image(message):
+    img = open("Images\\NoBitches.png", "rb")
+    bot.send_photo(message.chat.id, img)
+
 while True:
     try:
         bot.polling()
